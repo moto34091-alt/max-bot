884d8830e41aac4b02d59816890b171706369017
Support Vimba boss <moto34091@gmail.com>
2026-05-17 22:59:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-signals.js
-market.js
-server.js
-.env
-package.json
+require("dotenv").config();
+
+const TelegramBot = require("node-telegram-bot-api");
+const { smartSignal } = require("./signals");
+const { generateCloses } = require("./market");
+
+const mongoose = require("mongoose");
+const Signal = require("./models/Signal");
+
+mongoose.connect(process.env.MONGO_URL);
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🚀 OTC BOT READY", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "GET SIGNAL", callback_data: "signal" }]
+      ]
+    }
+  });
+});
+
+bot.on("callback_query", async (query) => {
+  const chatId = query.message.chat.id;
+
+  if (query.data === "signal") {
+    bot.sendMessage(chatId, "Analyzing market...");
+
+    setTimeout(async () => {
+      const closes = generateCloses();
+      const result = smartSignal(closes);
+
+      await Signal.create(result);
+
+      bot.sendMessage(chatId,
+`${result.signal}
+Confidence: ${result.confidence}%`
+      );
+
+    }, 2000);
+  }
+});
